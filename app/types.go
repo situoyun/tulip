5b07b9711b537f38a712e9c244bfc5d48f6391b2
yptian <1170492028@qq.com>
2023-07-24 15:21:39 +0800

    fixe salt bug

--- app/types.go
+++ app/types.go
@@ -11,21 +11,21 @@ type Model struct {
 
 type User struct {
 	ID           uint       `gorm:"primary_key" json:"id,omitempty"`
-	UUID         string     `gorm:"column:uid;unique_index" sql:"default:''" json:"-"`
+	UUID         string     `gorm:"column:uid;unique_index" sql:"default:''" json:"uuid,omitempty"`
 	Name         string     `gorm:"column:name;index" sql:"default:''" json:"name,omitempty"`
 	Alias        string     `gorm:"column:alias;index" sql:"default:''" json:"alias,omitempty"`
 	Tel          string     `json:"tel,omitempty" gorm:"index" sql:"default:''"`
 	Mail         string     `json:"mail,omitempty" gorm:"index" sql:"default:''"`
-	OpenID       string     `sql:"default:''" json:"-"`
+	OpenID       string     `sql:"default:''" json:"open_id,omitempty"`
 	PassWord     string     `gorm:"column:password" sql:"not null" json:"password,omitempty"`
-	PassWordType int        `gorm:"column:password_type" sql:"default:0"` //0表示默认 1表示只用md5
-	Salt         string     `gorm:"column:salt" sql:"default:''" json:"-"`
+	PassWordType int        `gorm:"column:password_type" sql:"default:0" json:"pass_word_type,omitempty"` //0表示默认 1表示只用md5
+	Salt         string     `gorm:"column:salt" sql:"default:''" json:"salt,omitempty"`
 	Area         string     `gorm:"column:area" sql:"default:''" json:"area,omitempty"`
-	LoginType    uint       `gorm:"column:login_type" json:"-"` //1账号 2微信
-	InvitedCode  string     `gorm:"column:invited_code;index" sql:"default:''" json:"invited_code"`
-	ModifyPWD    bool       `gorm:"column:modify_pwd"`
+	LoginType    uint       `gorm:"column:login_type" json:"-,omitempty"` //1账号 2微信
+	InvitedCode  string     `gorm:"column:invited_code;index" sql:"default:''" json:"invited_code,omitempty"`
+	ModifyPWD    bool       `gorm:"column:modify_pwd" json:"modify_pwd,omitempty"`
 	Avatar       string     `sql:"default:''" json:"avatar,omitempty"`
-	Sex          string     `sql:"default:''"  json:"sex,omitempty"`
+	Sex          string     `sql:"default:''" json:"sex,omitempty"`
 	IdNumber     string     `gorm:"type:varchar(100);default:''" json:"id_number,omitempty"`
 	Hobby        string     `json:"hobby,omitempty"`
 	Birth        string     `json:"birth,omitempty"`
